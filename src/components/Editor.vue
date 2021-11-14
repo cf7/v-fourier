eaadbe827b739fdab8670a6326e82bf79de4ad13
cf7 <cf7@users.noreply.github.com>
2021-11-14 04:58:54 -0800

    tests and file structure

---
+++ src/components/Editor.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-container class="app-view">
+    <b-row>
+      <b-col>
+        Test
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+
+
+<script>
+export default {
+  name: "Editor",
+  props: {
+    editorFontSize: Number,
+    editorFontSizes: Array,
+    displayText: String
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Panel.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+  </div>
+</template>
+
+
+
+<script>
+  export default {
+    name: "Panel",
+    props: {},
+  }
+</script>
+
+<style>
+
+</style>
